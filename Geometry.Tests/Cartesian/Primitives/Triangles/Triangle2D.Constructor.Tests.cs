07a7ab4a49668e30d86eb558f65315dc1edc6f90
Davis Brown <Davis.Brown@gentex.com>
2026-07-22 17:41:19 -0400

    adding the start of a unit test library for geometry

---
+++ Geometry.Tests/Cartesian/Primitives/Triangles/Triangle2D.Constructor.Tests.cs
@@ -0,0 +1,109 @@
+﻿using Geometry.Cartesian.Points;
+using Geometry.Cartesian.Primitives.Triangles;
+using Geometry.Cartesian.Primitives.Triangles.Exceptions;
+using Geometry.Quantities.Angles;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Geometry.Tests.Cartesian.Primitives.Triangles
+{
+    [TestClass]
+    public sealed class Triangle2DTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldThrowWhen_VerticesNull()
+        {
+            // need to test all of the points individually, and the constructor
+            // verifies them in sequential order. 
+            IPoint2D? p1 = null;
+            IPoint2D? p2 = null;
+            IPoint2D? p3 = null;
+
+            // if A is null
+            Assert.Throws<ArgumentNullException>(() => new Triangle2D(p1, p2, p3));
+
+            // if B is null
+            p1 = new Point2D(0, 0);
+            Assert.Throws<ArgumentNullException>(() => new Triangle2D(p1, p2, p3));
+
+            // if C is null
+            p2 = new Point2D(1, 0);
+            Assert.Throws<ArgumentNullException>(() => new Triangle2D(p1, p2, p3));
+        }
+
+        [TestMethod]
+        public void Constructor_ShouldThrowWhen_VerticeAreCollinear()
+        {
+            IPoint2D p1 = new Point2D(0, 0);
+            IPoint2D p2 = new Point2D(1, 0);
+            IPoint2D p3 = new Point2D(2, 0);
+
+            Assert.Throws<DegenerateTriangleException>(() =>
+                new Triangle2D(p1, p2, p3));
+
+            IPoint2D A = new Point2D(0, 0);
+            IPoint2D B = new Point2D(0, 1);
+            IPoint2D C = new Point2D(0, 3);
+
+            Assert.Throws<DegenerateTriangleException>(() => new Triangle2D(A, B, C));
+
+            IPoint2D X = new Point2D(0, 0);
+            IPoint2D Y = new Point2D(1, 1);
+            IPoint2D Z = new Point2D(2, 2);
+
+            Assert.Throws<DegenerateTriangleException>(() => new Triangle2D(X, Y, Z));
+        }
+
+        [TestMethod]
+        public void Constructor_ShouldThrowWhen_VerticesAreDuplicates()
+        {
+            IPoint2D A = new Point2D(0, 0);
+            IPoint2D B = new Point2D(0, 0);
+            IPoint2D C = new Point2D(1, 0);
+
+            Assert.Throws<DegenerateTriangleException>(() =>
+                new Triangle2D(A, B, C));
+        }
+
+
+        [TestMethod]
+        public void Constructor_ShouldCalculateAngles()
+        {
+            IPoint2D A = new Point2D(0, 0);
+            IPoint2D B = new Point2D(1, 0);
+            IPoint2D C = new Point2D(0, 1);
+
+            Triangle2D t = new Triangle2D(A, B, C);
+            Assert.IsTrue( (Angle.Pi / 2).NearlyEquals(t.Theta1) );
+            Assert.IsTrue( (Angle.Pi / 4).NearlyEquals(t.Theta2) );
+            Assert.IsTrue( (Angle.Pi / 4).NearlyEquals(t.Theta3) );
+        }
+
+
+        [TestMethod]
+        public void Constructor_ShouldCalculateArea()
+        {
+            IPoint2D A = new Point2D(0, 0);
+            IPoint2D B = new Point2D(4, 0);
+            IPoint2D C = new Point2D(0, 3);
+
+            Triangle2D t = new Triangle2D(A, B, C);
+
+            Assert.AreEqual(6.0, t.Area, 1e-12);
+            Assert.AreEqual(6.0, t.SignedArea, 1e-12);
+        }
+
+
+        [TestMethod]
+        public void Constructor_ShouldCalculateNegativeSignedAreaWhen_Clockwise()
+        {
+            IPoint2D A = new Point2D(0, 0);
+            IPoint2D B = new Point2D(0, 3);
+            IPoint2D C = new Point2D(4, 0);
+
+            Triangle2D t = new Triangle2D(A, B, C);
+
+            Assert.AreEqual(6.0, t.Area, 1e-12);
+            Assert.AreEqual(-6.0, t.SignedArea, 1e-12);
+        }
+    }
+}
---
+++ Geometry.Tests/Cartesian/Primitives/Triangles/Triangle3D.Constructor.Tests.cs
@@ -0,0 +1,85 @@
+﻿using Geometry.Cartesian.Points;
+using Geometry.Cartesian.Primitives.LineSegments;
+using Geometry.Cartesian.Primitives.Triangles;
+using Geometry.Cartesian.Primitives.Triangles.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry.Tests.Cartesian.Primitives.Triangles
+{
+    [TestClass]
+    public sealed class Triangle3DTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldThrowWhen_VerticesNull()
+        {
+            // need to test all of the points individually, and the constructor
+            // verifies them in sequential order. 
+            IPoint3D? p1 = null;
+            IPoint3D? p2 = null;
+            IPoint3D? p3 = null;
+
+            // if A is null
+            Assert.Throws<ArgumentNullException>(() => new Triangle3D(p1, p2, p3));
+
+            // if B is null
+            p1 = new Point3D(0, 0, 0);
+            Assert.Throws<ArgumentNullException>(() => new Triangle3D(p1, p2, p3));
+
+            // if C is null
+            p2 = new Point3D(1, 0, 0);
+            Assert.Throws<ArgumentNullException>(() => new Triangle3D(p1, p2, p3));
+        }
+
+
+        [TestMethod]
+        public void Constructor_ShouldThrowWhen_VerticesAreCollinear()
+        {
+            IPoint3D A = new Point3D(0, 0, 0);
+            IPoint3D B = new Point3D(1, 0, 0);
+            IPoint3D C = new Point3D(2, 0, 0);
+
+            Assert.Throws<DegenerateTriangleException>(() => new Triangle3D(A, B, C));
+
+            A = new Point3D(0, 0, 0);
+            B = new Point3D(0, 1, 0);
+            C = new Point3D(0, 2, 0);
+            Assert.Throws<DegenerateTriangleException>(() => new Triangle3D(A, B, C));
+
+            A = new Point3D(0, 0, 0);
+            B = new Point3D(0, 0, 1);
+            C = new Point3D(0, 0, 2);
+            Assert.Throws<DegenerateTriangleException>(() => new Triangle3D(A, B, C));
+
+            A = new Point3D(0, 0, 0);
+            B = new Point3D(1, 1, 1);
+            C = new Point3D(2, 2, 2);
+            Assert.Throws<DegenerateTriangleException>(() => new Triangle3D(A, B, C));
+        }
+
+
+        [TestMethod]
+        public void Constructor_ShouldThrowWhen_VerticesAreDuplicates()
+        {
+            IPoint3D A = new Point3D(0, 0, 0);
+            IPoint3D B = new Point3D(0, 0, 0);
+            IPoint3D C = new Point3D(1, 1, 1);
+            Assert.Throws<DegenerateTriangleException>(() => new Triangle3D(A, B, C));
+        }
+
+
+        [TestMethod]
+        public void Constructor_ShouldCalculateArea()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void Constructor_ShouldCalculateNegativeSignedAreaWhen_Clockwise()
+        {
+
+        }
+    }
+}
